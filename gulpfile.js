0041fb106bd6b991daaa7034cc82e14a67f7531b
LeonardoSantosOliveira <leoleosantos21@gmail.com>
2023-11-03 20:39:07 -0300

    setup do projeto completo

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
+const imagemin = require('gulp-imagemin')
 
 function compilaSass(){
     return gulp.src('./src/styles/**/*.scss')
@@ -9,7 +10,13 @@ function compilaSass(){
         .pipe(gulp.dest('./dist/styles/main.css'))
 }
 
-exports.default = gulp.parallel(compilaSass)
+function comprimeImagem(){
+    return gulp.src('./src/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images/'))
+}
+
+exports.default = gulp.parallel(compilaSass, comprimeImagem)
 
 exports.watch = function(){
     return gulp.watch('./src/styles/**/*.scss', gulp.parallel(compilaSass))
